e33eabc83705f55903075545ff452de9b7915be2
zhenxing <2083481778@qq.com>
2022-10-06 22:04:51 +0800

    fix

--- provider/provider-plus/src/main/java/com/byx/study/plus/PlusProviderApplication.java
+++ provider/provider-plus/src/main/java/com/byx/study/plus/PlusProviderApplication.java
@@ -1,20 +1,14 @@
 package com.byx.study.plus;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.FactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.DispatcherServlet;
-
-import java.util.HashMap;
 
 @SpringBootApplication
-public class PlusProviderApplciation {
+public class PlusProviderApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PlusProviderApplciation.class, args);
+        SpringApplication.run(PlusProviderApplication.class, args);
     }
 
     @Bean
